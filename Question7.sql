f1327e76db5d49631c05036a0f700b4a0eef6ee3
Anna Heston <amheston@gmail.com>
2016-10-09 21:51:05 -0700

    Test Query 7

---
+++ Question7.sql
@@ -0,0 +1,3 @@
+SELECT book_authors.authorname, book.title, book_copies.no_of_copies, library_branch.branchname
+From book_authors left outer join book on book_authors.bookid = book.bookid left outer join book_copies on book.bookid = book_copies.bookid Join book_loans on book.bookid= book_loans.bookid join library_branch ON library_branch.branchid = book_loans.branchid
+Where authorname = 'Stephen_King' AND Library_branch.branchname = 'Central'
