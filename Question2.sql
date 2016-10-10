979d420e874f2297e1ca53c792b408a5f9be93fb
Anna Heston <amheston@gmail.com>
2016-10-09 21:48:49 -0700

    Test Query 2

---
+++ Question2.sql
@@ -0,0 +1,4 @@
+SELECT title, branchname, no_of_copies
+FROM Book JOIN Book_Copies ON book.bookid = book_copies.bookid Inner Join Library_Branch ON Book_copies.branchid = Library_branch.branchid
+
+where book.title = 'the_Lost_Tribe' 
